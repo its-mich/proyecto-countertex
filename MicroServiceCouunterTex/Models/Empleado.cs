55567caf40333936df82d4c744631dc1b2bb620b
Joseasegura <joseavelinosegurabociga@gmail.com>
2024-10-17 17:22:52 -0500

    Si agrega Controlador con verbos HTTP Para empleadoSe agrego cambio en appsettings.json

---
+++ MicroServiceCouunterTex/Models/Empleado.cs
@@ -0,0 +1,12 @@
+﻿namespace MicroServiceCounterTex.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public DateOnly FechaContrato { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
