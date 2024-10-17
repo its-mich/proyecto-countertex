e422b91e7a4f996bc45c34b62adc6e31e233f6d4
Billy5000x <jhoansnf02@gmail.com>
2024-10-17 16:22:56 -0500

    se agrego cadena de conexion en el archiovo appsettings.json se crea carpeta data con db contexAgregar archivos de proyecto.

---
+++ MicroServiceCouunterTex/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore; 
+
+namespace MicroServiceCouunterTex.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public int IdEmpleado { get; set; }
+        public string Correo { get; set; }
+        public string Pasword { get; set; }
+    }
+}
